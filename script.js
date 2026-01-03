17c409958c261596b5bcfb67a6b0ae0bf9983e2a
Koundinyeah <laxmikoundinya@gmail.com>
2026-01-03 15:44:44 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Laxmi Koundinya Portfolio Loaded");
+function openModal(project) {
+    const modal = document.getElementById("modal");
+    const text = document.getElementById("modal-text");
+
+    if (project === "isl") {
+        text.innerText =
+            "ISL Gesture Translation: Detects hand gestures and converts them into readable text for assistive communication.";
+    } else {
+        text.innerText =
+            "Smart Warehouse Asset Tracker: Tracks temperature, humidity, and asset status with real-time dashboard analytics.";
+    }
+
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
